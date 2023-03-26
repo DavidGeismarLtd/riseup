d222ea6557d6cdf2d97868c7338aa3e14c170db1
davidgeismarltd <davidgeismar@yunoo.io>
2023-03-26 16:09:14 +0200

    create session group subscription

--- lib/rise_up/client/session_group_subscriptions.rb
+++ lib/rise_up/client/session_group_subscriptions.rb
@@ -3,6 +3,18 @@ module RiseUp
     module SessionGroupSubscriptions
       BASE = '/sessiongroupsubscriptions'
 
+      def create_session_group_subscription(user_id, session_group_id, options = {})
+        request(ApiResource::SessionGroupSubscription) do
+          self.class.post(BASE, {
+                            body: options.merge(iduser: user_id, idsessiongroup: session_group_id).to_json,
+                            headers: {
+                              'Authorization' => "Bearer #{access_token}",
+                              'Content-Type' => 'application/json'
+                            }
+                          }).body
+        end
+      end
+
       def retrieve_session_group_subscriptions(options = {})
         request(ApiResource::Session) do
          self.class.get(BASE, {
