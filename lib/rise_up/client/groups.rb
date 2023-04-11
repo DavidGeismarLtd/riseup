36d73d2cf91d6bc69e2b12df3ed3b567519f7eb9
davidgeismarltd <davidgeismar@yunoo.io>
2023-04-11 14:22:32 +0200

    Remove register users to group

--- lib/rise_up/client/groups.rb
+++ lib/rise_up/client/groups.rb
@@ -6,15 +6,41 @@ module RiseUp
       BASE = '/groups'
 
       def register_users_to_group(group_id, users=[])
-        self.class.post("#{BASE}/#{group_id}/subscribe", {
-                          body: users.to_json,
-                          headers: {
-                            'Authorization' => "Bearer #{access_token}",
-                            'Content-Type' => 'application/json'
-                          }
-                        }).body
+        request(ApiResource::User) do
+          self.class.post("#{BASE}/#{group_id}/subscribe", {
+                            body: users.to_json,
+                            headers: {
+                              'Authorization' => "Bearer #{access_token}",
+                              'Content-Type' => 'application/json'
+                            }
+                          }).body
+        end
       end
 
+      def register_users_to_group(group_id,options = {})
+        request(ApiResource::User) do
+          self.class.post("#{BASE}/#{group_id}/subscribe", {
+            body: options.to_json,
+            headers: {
+              'Authorization' => "Bearer #{access_token}",
+              'Content-Type' => 'application/json'
+            }
+          }).body
+        end
+      end
+
+      def unregister_users_to_group(group_id,options = {})
+        request(ApiResource::User) do
+          self.class.post("#{BASE}/#{group_id}/unsubscribe", {
+            body: options.to_json,
+            headers: {
+              'Authorization' => "Bearer #{access_token}",
+              'Content-Type' => 'application/json'
+            }
+          }).body
+        end
+      end
+      
       def retrieve_groups(options = {})
         request(ApiResource::Group) do
          self.class.get(BASE, {
