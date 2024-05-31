ba7921dd8757cae6637495908686aa16345d26b2
davidgeismarltd <davidgeismar@yunoo.io>
2024-05-31 20:01:22 +0200

    Avoid invalid attr name

--- lib/rise_up/api_resource/resource.rb
+++ lib/rise_up/api_resource/resource.rb
@@ -1,14 +1,26 @@
 # frozen_string_literal: true
+require 'logger'
 
 module RiseUp
   module ApiResource
     # base resource class
     class Resource
       def initialize(options = {})
-        options.map do |(k, v)|
-          self.class.attr_accessor(k)
-          send("#{k}=", v)
+        @logger = Logger.new(STDOUT)
+        options.each do |k, v|
+          if valid_attribute_name?(k)
+            self.class.attr_accessor(k)
+            send("#{k}=", v)
+          else
+            @logger.warn("Invalid attribute name `#{k}`")
+          end
         end
+      end
+
+      private
+
+      def valid_attribute_name?(name)
+        name.to_s.match?(/\A[a-z_][a-zA-Z_0-9]*\z/)
       end
     end
   end
